d82550ef95f1f02c93889d957d30e0d2cf574e6f
jpenaunilibre <jandy.pena.ttc.506@unilibre.edu.co>
2024-11-13 21:44:42 -0500

    .

--- script-3.js
+++ script-3.js
@@ -3,9 +3,9 @@ let monto = parseInt(prompt("Digite el monto total de su compra"));
 if(monto > 100 ){
 
     let porcentaje = monto * 0.9;
-    document.write("¡Su compra tiene un descuento del 10%! ", porcentaje, "<br>" )
+    document.write("¡Su compra tiene un descuento del 10%! <br> ", porcentaje )
 }else {
 
-    document.write("Su compra no aplica para el descuento ", monto, "<br>")
+    document.write("Su compra no aplica para el descuento <br>", monto)
 
 }
